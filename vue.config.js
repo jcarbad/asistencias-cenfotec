809180ecd1fc0f4e0e4d6046ff114dfbc1fa99f1
Armando Carballo <jcarbad@gmail.com>
2024-07-31 19:49:01 -0600

    Habilita deployment a traves de plugin S3

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+
   css: {
     loaderOptions: {
       scss: {
@@ -10,4 +11,26 @@ module.exports = defineConfig({
       },
     },
   },
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      overrideEndpoint: false,
+      region: 'us-east-1',
+      bucket: 'asistencias-cenfotec',
+      createBucket: false,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5
+    }
+  }
 });
