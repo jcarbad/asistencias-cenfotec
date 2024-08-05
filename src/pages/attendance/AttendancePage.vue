4e9be991ea92424cdaa818dc19a9e10aed5b21f4
Armando Carballo <jcarbad@gmail.com>
2024-08-05 15:33:51 -0600

    Mock de pantalla de asistencia

--- src/pages/attendance/AttendancePage.vue
+++ src/pages/attendance/AttendancePage.vue
@@ -8,35 +8,92 @@
         <section id="seleccion-grupo">
           <div id="seleccion-grupo-ext" class="container">
             <div id="seleccion-grupo-int" class="row">
-              <div id="escoger-grupo" class="col-6">
-                <DropdownOptions :title="'Asignatura'" :itemValues="todasAsignaturas"/>
-                <DropdownOptions :title="'Nivel'" :itemValues="todosNiveles"/>
-                <DropdownOptions :title="'Grupo'" :itemValues="todosGrupos"/>
+              <div id="escoger-grupo" class="col-4">
+                <div class="row">
+                  <DropdownOptions :title="'Asignatura'" :itemValues="todasAsignaturas"/>
+                </div>
+                <div class="row">
+                  <DropdownOptions :title="'Nivel'" :itemValues="todosNiveles"/>
+                </div>
+                <div class="row">
+                  <DropdownOptions :title="'Grupo'" :itemValues="todosGrupos"/>
+                </div>
+                <div id="boton-listar" class="row pt-5">
+                  <MultiUseButton :textValue="'Listar'" :buttonType="'primary'" />
+                </div>
               </div>
-              <div id="escoger-fecha" class="col-6 container">
-                  <div id="fecha-picker" class="row">
-                    (( DATE PICKER ))
+              <div id="escoger-fecha" class="col-4">
+                  <div id="fecha-picker" class="row col-12">
+                    <VueDatePicker
+                        v-model="date"
+                        locale="es"
+                        inline
+                        :disabled-week-days="[6, 0]"
+                        :enable-time-picker="false"
+                    ></VueDatePicker>
                   </div>
-                  <div id="boton-listar" class="row">
-                    <MultiUseButton :textValue="'Listar'" :buttonType="'primary'" />
-                  </div>
-
               </div>
             </div>
           </div>
         </section>
-        <section id="asistencia-grupo"></section>
+        <section id="asistencia-grupo">
+          <div id="listado-asistencia" class="container d-flex justify-content-center align-items-center full-height">
+            <DataTable :tableHeaders="tableHeaders" additionalInformation>
+              <template #dataRows>
+                <tr v-for="(item, index) in tableData" :key="index">
+                  <td>{{ item.id }}</td>
+                  <td>{{ item.nombre }}</td>
+                  <td>{{ item.apellido1 }}</td>
+                  <td>{{ item.apellido2 }}</td>
+                  <td>
+                    <div id="icono-asistencia" class="d-flex justify-content-center align-items-center full-height">
+                      <img v-if="item.asistencia === 'presente'" src="@/assets/checkmark2.png" style="width: 32px; height: 32px;" alt="icono de estudiante presente" />
+                      <img v-if="item.asistencia === 'justificada'" src="@/assets/justificada.png" style="width: 32px; height: 32px;" alt="icono de ausencia justificada" />
+                      <img v-if="item.asistencia === 'injustificada'" src="@/assets/question-mark.png" style="width: 32px; height: 32px;" alt="icono de ausencia injustificada" />
+                      <img v-if="item.asistencia === ''" src="@/assets/empty.png" style="width: 32px; height: 32px;" alt="icono de asistencia de estudiante" />
+                    </div>
+                  </td>
+                  <td>
+                      <div id="radio-seleccion-asistencia">
+                        <div>
+                          <label>
+                            <input type="radio" v-model="item.asistencia" value="presente" />
+                            Presente
+                          </label>
+                        </div>
+                        <div>
+                          <label>
+                            <input type="radio" v-model="item.asistencia" value="justificada" />
+                            Ausencia Justificada
+                          </label>
+                        </div>
+                        <div>
+                          <label>
+                            <input type="radio" v-model="item.asistencia" value="injustificada" />
+                            Ausencia Injustificada
+                          </label>
+                        </div>
+                      </div>
+                  </td>
+                </tr>
+              </template>
+            </DataTable>
+          </div>
+        </section>
       </main>
     </template>
   </MainLayout>
 </template>
 
 <script lang="ts" setup>
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 import MainLayout from "@/layouts/MainLayout.vue";
 import DropdownOptions from "@/components/dropdownOptions/DropdownOptions.vue";
 import {IDataTableInfo} from "@/models/IDataTableInfo";
 import {ref} from "vue";
 import MultiUseButton from "@/components/multiUseButton/MultiUseButton.vue";
+import DataTable from "@/components/dataTable/DataTable.vue";
 
 const todasAsignaturas = ref([
   {id: "1", value: "Español"},
@@ -61,6 +118,59 @@ const todosGrupos: IDataTableInfo[] = [
   {id: "4", value: "Cuatro"},
   {id: "5", value: "Cinco"}
 ];
+
+const tableHeaders : IDataTableInfo[] = [
+  {
+    id: "id",
+    value: "Identificación",
+  },
+  {
+    id: "nombre",
+    value: "Nombre",
+  },
+  {
+    id: "apellido_1",
+    value: "PrimerApellido",
+  },
+  {
+    id: "apellido_2",
+    value: "Segundo Apellido",
+  },
+  {
+    id: "estado",
+    value: "Estado",
+  },
+  {
+    id: "asistencia",
+    value: "Asistencia",
+  }
+];
+
+const tableData = ref([
+  {
+    id: "111111111",
+    nombre: "Armando",
+    apellido1: "Ayala",
+    apellido2: "Córdoba",
+    asistencia: ''
+  },
+  {
+    id: "222222222",
+    nombre: "Leonardo",
+    apellido1: "Araya",
+    apellido2: "Parajeles",
+    asistencia: ''
+  },
+  {
+    id: "333333333",
+    nombre: "Joan",
+    apellido1: "Carballo",
+    apellido2: "Badilla",
+    asistencia: ''
+  },
+]);
+
+const date = ref();
 
 </script>
 
